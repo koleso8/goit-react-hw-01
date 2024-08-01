9f9d242db6a3c5be8118bc05c4386105ad959258
koleso8 <dev.koleso@gmail.com>
2024-08-01 16:43:31 +0300

    add 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,18 @@
-import { Product } from './Product';
+import userData from '../userData.json';
+import { Profile } from './Profile';
 
-export default function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Best selling</h1>
-
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
       />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-    </div>
+    </>
   );
-}
+};
+
+export default App;
